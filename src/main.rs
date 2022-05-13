5e6b46f303bc86d693e75c9616e0527c7d1274b6
YoavNir99 <cocoos9@gmail.com>
2022-05-13 16:31:29 +0300

    day 16 part 2

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ fn main() {
     let sender : Vec<(String,u16)> = vec!(("children",3),("cats",7),("samoyeds",2),("pomeranians",3),("akitas",0),("vizslas",0),("goldfish",5),("trees",3),("cars",2),("perfumes",1))
                                         .iter().map(|x| (x.0.to_owned(),x.1)).collect();
 
-    println!("{:?}",sender);
-
     for line in lines.lines() {
         let line = match line {
             Ok(line) => line,
@@ -24,6 +22,25 @@ fn main() {
     }
 }
 
+fn match_exp(clue:&(String,u16),exp:&(String,u16)) -> bool {
+    if clue.0 == exp.0 {
+        if clue.0 == "cats" || clue.0 == "trees" {
+            if clue.1>=exp.1 {
+                return true
+            }
+        } else if clue.0 == "pomeranians" || clue.0 == "goldfish" {
+            if clue.1<=exp.1 {
+                return true
+            }
+        } else {
+            if clue.1!=exp.1 {
+                return true
+            }
+        }
+    }
+    false
+}
+
 fn parse_line(line:String,sender:&Vec<(String,u16)>) -> (bool,u16) {
     let line : Vec<String> = line.split(" ").map(|x| x.to_owned()).collect();
 
@@ -34,13 +51,7 @@ fn parse_line(line:String,sender:&Vec<(String,u16)>) -> (bool,u16) {
     let exp3 = (line[6].clone(),line[7].clone().parse::<u16>().unwrap());
     
     for clue in sender {
-        if clue.0 == exp1.0 && clue.1!=exp1.1 {
-            return (false,0)
-        }
-        if clue.0 == exp2.0 && clue.1!=exp2.1 {
-            return (false,0)
-        }
-        if clue.0 == exp3.0 && clue.1!=exp3.1 {
+        if match_exp(clue, &exp1) || match_exp(clue, &exp2) || match_exp(clue, &exp3) {
             return (false,0)
         }
     }
