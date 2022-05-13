417e07e10a64466064dc6c0af133c4d6d966cadf
YoavNir99 <cocoos9@gmail.com>
2022-05-13 16:06:20 +0300

    day 16 part 1

--- src/main.rs
+++ src/main.rs
@@ -6,78 +6,46 @@ use std::io::prelude::*;
 fn main() {
     let file = open("/Users/yoavnir/Documents/vs code/rust/old/solving_challenges/advent_of_code/text.txt");
     let lines = file_to_iter(file);
-    let mut ings:Vec<Ing> = Vec::new();
+    let sender : Vec<(String,u16)> = vec!(("children",3),("cats",7),("samoyeds",2),("pomeranians",3),("akitas",0),("vizslas",0),("goldfish",5),("trees",3),("cars",2),("perfumes",1))
+                                        .iter().map(|x| (x.0.to_owned(),x.1)).collect();
+
+    println!("{:?}",sender);
+
     for line in lines.lines() {
         let line = match line {
             Ok(line) => line,
             Err(err) => panic!("{}",err)
         };
 
-        parse_line(line,&mut ings)
-    }
-
-    let mut biggest = 0;
-
-    for i1 in 0..101 {
-        for i2 in 0..101-i1 {
-            for i3 in 0..101-i1-i2 {
-                let i4=100-i1-i2-i3;
-                let calc = calc_comb([i1,i2,i3,i4], &ings);
-                if calc>biggest {
-                    biggest=calc
-                }
-            }
+        match parse_line(line,&sender) {
+            (true,num) => {println!("{num}");break},
+            _ => ()
         }
     }
-
-    println!("{biggest}")
 }
 
-fn calc_comb(comb:[i64;4],ings:&Vec<Ing>) -> i64 {
-    let mut c = 0;
-    let mut d = 0;
-    let mut f = 0;
-    let mut t = 0;
-    let mut cals = 0;
-
-    for idx in 0..4 {
-        c += ings[idx].capacity*comb[idx];
-        d += ings[idx].durab*comb[idx];
-        f += ings[idx].flav*comb[idx];
-        t += ings[idx].texture*comb[idx];
-        cals += ings[idx].cals*comb[idx];
-    }
-    if c < 0 {c=0};
-    if d < 0 {d=0};
-    if f < 0 {f=0};
-    if t < 0 {t=0};
-
-
-    return c*d*f*t
-
-    
-}
-
-#[derive(Debug,Clone)]
-struct Ing {
-    name : String,
-    capacity : i64,
-    durab : i64,
-    flav : i64,
-    texture : i64,
-    cals : i64
-}
-
-fn parse_line(line:String,arr:&mut Vec<Ing>) {
+fn parse_line(line:String,sender:&Vec<(String,u16)>) -> (bool,u16) {
     let line : Vec<String> = line.split(" ").map(|x| x.to_owned()).collect();
-    arr.push(
-        Ing {
-            name : line[0].clone(),
-            capacity : line[2].parse().unwrap(),
-            durab :  line[4].parse().unwrap(),
-            flav : line[6].parse().unwrap(),
-            texture :  line[8].parse().unwrap(),
-            cals :  line[10].parse().unwrap(),
+
+    let sue_num = line[1].clone().parse().unwrap();
+
+    let exp1 = (line[2].clone(),line[3].clone().parse::<u16>().unwrap());
+    let exp2 = (line[4].clone(),line[5].clone().parse::<u16>().unwrap());
+    let exp3 = (line[6].clone(),line[7].clone().parse::<u16>().unwrap());
+    
+    for clue in sender {
+        if clue.0 == exp1.0 && clue.1!=exp1.1 {
+            return (false,0)
         }
-    )
+        if clue.0 == exp2.0 && clue.1!=exp2.1 {
+            return (false,0)
+        }
+        if clue.0 == exp3.0 && clue.1!=exp3.1 {
+            return (false,0)
+        }
+    }
+    
+    (true,sue_num)
 }
+
+
