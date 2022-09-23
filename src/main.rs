f32c8cb08e88d57934185496aa3348004cb8b857
YoavNir99 <cocoos9@gmail.com>
2022-09-23 14:15:16 +0300

    day 21 part 1

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::io::prelude::*;
 //use std::ops::Add;
 
 fn main() {
+    /*
     let file = open("/Users/yoavnir/Documents/vs code/rust/old/solving_challenges/advent_of_code/text.txt");
     let lines = file_to_iter(file);
-    let mut boxes : Vec<u16> = Vec::new();
 
     for line in lines.lines() {
         let line = match line {
@@ -14,65 +14,116 @@ fn main() {
             Err(err) => panic!("{}",err)
         };
 
-        parse_line(line,&mut boxes)
     }
+    */
+    
+    let mut player = Player::new();
+    let mut boss = Boss {
+        health : 103,
+        damage : 9,
+        armor : 2
+    };
 
-    let mut solutions : Vec<Vec<usize>> = Vec::new();
-    let mut solution : Vec<usize> = Vec::new();
+    let weapons = [
+        [0,0],
+        [8,4],
+        [10,5],
+        [25,6],
+        [40,7],
+        [74,8]
+    ];
 
-    solve_recursive(&mut solutions, &boxes,&mut solution,0);
+    let armors = [
+        [0,0],
+        [13,1],
+        [31,2],
+        [53,3],
+        [75,4],
+        [102,5]
+    ];
 
-    for sol in &mut solutions {
-        sol.sort();
-    }
+    let rings: [[i32;3];8] = [
+        [0,0,0],
+        [0,0,0],
+        [25,1,0],
+        [50,2,0],
+        [100,3,0],
+        [20,0,1],
+        [40,0,2],
+        [80,0,3]
+    ];
 
-    solutions.dedup();
+    let mut cheapest = 200;
 
-    solutions.sort_by_key(|x| x.len());
-
-    let mut solutions_len = Vec::new();
-
-    for sol in &mut solutions {
-        solutions_len.push(sol.len())
-    }
-
-    let shortest = solutions_len[0];
-
-    solutions_len = solutions_len.iter().filter(|x| *x==&shortest).map(|x| *x).collect();
-
-    println!("{:?}",&solutions_len.len());
-}
-
-fn parse_line(line:String,boxes:&mut Vec<u16>) {
-    //let line : Vec<String> = line.split(" ").map(|x| x.to_owned()).collect();
-    let num : u16= line.parse().unwrap();
-    boxes.push(num)
-}
-
-fn solve_recursive(solutions : &mut Vec<Vec<usize>>,boxes : &Vec<u16>, solution :&mut Vec<usize>,curr_idx:usize) {
-    let sum = sum_idx(boxes, solution);
-    if sum > 150 {
-        return
-    } else if sum == 150 {
-        solutions.push(solution.clone());
-        return
-    }
-    else {
-        for idx in curr_idx..20 {
-            if !solution.contains(&idx) {
-                solution.push(idx);
-                solve_recursive(solutions, boxes, solution,idx);
-                solution.pop();
+    for weapon in weapons {
+        for armor in armors {
+            for ring1 in 0..8 {
+                for ring2 in 0..8 {
+                    if ring1 != ring2 {
+                        let cost = weapon[0]+armor[0]+rings[ring1][0]+rings[ring2][0];
+                        player.armor = armor[1]+rings[ring1][2]+rings[ring2][2];
+                        player.damage = weapon[1]+rings[ring1][1]+rings[ring2][1];
+                        if game(&player, &boss) && cost < cheapest {
+                            cheapest = cost
+                        }
+                    }
+                }
             }
         }
     }
-    //println!("{:?}",solutions);
+
+    println!("{cheapest}")
 }
 
-fn sum_idx(boxes:&Vec<u16>,solution:&Vec<usize>) -> u16 {
-    let mut ans = 0;
-    for idx in solution {
-        ans+=boxes[*idx]
+fn game(player : &Player, boss : &Boss) -> bool {
+    let mut player = player.clone();
+    let mut boss = boss.clone();
+
+    loop {
+        // player attack
+        if player.damage-boss.armor >= 1 {
+            boss.health -= player.damage-boss.armor;
+        } else {
+            boss.health -= 1;
+        }
+        if boss.health <= 0 {
+            return true
+        }
+        // boss attack
+        if boss.damage-player.armor >= 1 {
+            player.health -= boss.damage-player.armor;
+        } else {
+            player.health -= 1;
+        }
+        if player.health <= 0 {
+            return false
+        }
     }
-    return ans
 }
+
+#[derive(Clone)]
+struct Player {
+    health : i32,
+    armor : i32,
+    damage : i32,
+    coins : i32,
+}
+
+impl Player {
+    fn new() -> Player {
+        return Player { health: 100, armor: 0, damage: 0, coins: 0 }
+    }
+}
+
+#[derive(Clone)]
+struct Boss {
+    health : i32,
+    armor : i32,
+    damage : i32
+}
+
+/*
+fn parse_line(line:String) {
+    //let line : Vec<String> = line.split(" ").map(|x| x.to_owned()).collect();
+}
+*/
