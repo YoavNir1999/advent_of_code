255d91ed992be8d89d4d3bf57779e3a7d0d0d212
YoavNir99 <cocoos9@gmail.com>
2023-02-05 16:42:48 +0200

    2022 day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -7,58 +7,34 @@ fn main() {
     let file = open("/Users/yoavnir/Documents/vs code/rust/old/solving_challenges/advent_of_code/text.txt");
     let lines = file_to_iter(file);
 
-    let mut packages : Vec<u64> = Vec::new();
-
-    let mut best = (100,1000000000);
+    let mut highest = [0,0,0];
+    let mut current : i32 = 0;
 
     for line in lines.lines() {
         let line = match line {
             Ok(line) => line,
             Err(err) => panic!("{}",err)
         };
-        packages.push(parse_line(line))
-    }
 
-    let group_weight : u64 = packages.iter().sum::<u64>()/4;
-    let mut main : Vec<u64> = Vec::new();
-
-    recursive_add(&mut packages, 0,&mut best,group_weight,&mut main);
-
-    println!("{:?}",packages);
-
-    /*
-    println!("{:?}",configs);
-
-    let res = configs.best_config();
-    println!("{res}")
-    */
-
-
-}
-
-fn recursive_add(packages :&Vec<u64>,idx : usize, best : &mut (usize,u64),weight : u64, main : &mut Vec<u64>) {
-    if idx < packages.len() {
-        let current = packages[idx];
-        let idx = idx + 1;
-        // include current
-        main.push(current);
-        recursive_add(packages, idx, best, weight, main);
-        // don't include current
-        main.pop();
-        recursive_add(packages, idx, best, weight, main);
-
-    } else if main.iter().sum::<u64>() == weight {
-        let length = main.len();
-        let qe : u64 = main.iter().product();
-        if length < best.0 {
-            *best = (length,qe);
-            println!("{:?}",best);
-        } else if length == best.0 && qe < best.1 {
-            *best = (length,qe);
-            println!("{:?}",best);
+        if line.is_empty() {
+            if current > highest[0] {
+                highest[2] = highest[1];
+                highest[1] = highest[0];
+                highest[0] = current;
+            } else if current > highest[1] {
+                highest[2] = highest[1];
+                highest[1] = current
+            } else if current > highest[2] {
+                highest[2] = current;
+            }
+            current = 0;
+        } else {
+            current += line.parse::<i32>().unwrap()
         }
+        
     }
-    
+
+    println!("{}",highest[0]+highest[1]+highest[2]);
 
 }
 
